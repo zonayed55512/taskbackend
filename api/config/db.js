15787ffbb397ecedebcc3f5679c8ae30d06aa1d7
zonayed55512 <118248769+zonayed55512@users.noreply.github.com>
2023-01-31 01:05:08 +0600

    Upgration 2

---
+++ api/config/db.js
@@ -0,0 +1,27 @@
+let mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function mongoConnection() {
+  try {
+    let mongoString = process.env.MONGO_STRING;
+    let conn = await mongoose.connect(mongoString, {
+      dbName: "simpletask",
+      // user:'',
+      // pass:'',
+      useNewUrlParser: true,
+    });
+
+    console.log(
+      `Database Connect Successfully to host : ${conn.connection.host}`
+        .bgMagenta.black
+    );
+  } catch (err) {
+    console.log(`${err.message}`.bgRed.white);
+    // process.exit(1);
+  }
+}
+
+module.exports = mongoConnection;
